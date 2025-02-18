6ad2a36eef5778b46c4fcbc374eafba36f6433f5
anu-manian <anu.bai24@rathinam.com>
2025-02-18 15:42:12 +0530

    C

---
+++ student details.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+
+int main()
+{
+
+    char name[]="Anu";
+    printf("Name:%s\n",name);
+    char DOB[]="10.10.2006";
+    printf("Date of Birth:%s\n",DOB);
+    int a=17;
+    printf("Age:%d\n",a);
+    char f[]="Manian";
+    printf("Father's Name:%s\n",f);
+    char m[]="Uma devi";
+    printf("Mother's Name:%s\n",m);
+    int b=443;
+    printf("10th std mark:%d\n",b);
+    int c=453;
+    printf("12th std mark:%d\n",c);
+    char college[]="Rathinam Technical Campus";
+    printf("college:%s\n",college);
+    char d[]="Under Graduate";
+    printf("Degree:%s\n",d);
+    char course[]="Btech Artificial Intelligence & Data Science";
+    printf("Department:%s\n",course);
+    return 0;
+}
